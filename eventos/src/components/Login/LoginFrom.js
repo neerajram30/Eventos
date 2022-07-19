c8f714048e3ad80c22d20e96bafeb5383da4b6b5
neeraj <neerajramachandran30@gmail.com>
2022-07-19 22:04:07 +0530

    updated login form

--- eventos/src/components/Login/LoginFrom.js
+++ eventos/src/components/Login/LoginFrom.js
@@ -7,7 +7,7 @@ function LoginFrom() {
         <h2>Login</h2>
       </div>
       <form className='md:mt-10 mt-6 md:w-80 w-full'>
-      <input type="text" placeholder='Email' name="username" className='pl-4 pr-4 h-10 md:w-80 w-full shadow-[#000000c9] shadow-formbox text-sm rounded-sm'/><br/>
+      <input type="text" placeholder='Email' name="email" className='pl-4 pr-4 h-10 md:w-80 w-full shadow-[#000000c9] shadow-formbox text-sm rounded-sm'/><br/>
       <input type="password"  placeholder='Password' name="password" className='pl-4 pr-4 mt-5 h-10 md:w-80 w-full shadow-[#000000c9] shadow-formbox text-sm rounded-sm'/><br/>
       <input type="submit" name="submit" value="Login" className='md:w-80 w-full h-10 text-sm bg-blue text-white mt-10 rounded-sm font-extrabold'/>
       </form>
