031657f7a0ffb44c632053c58154ce93cac6496b
neeraj <neerajramachandran30@gmail.com>
2022-07-21 01:27:27 +0530

    new links added in navbar

--- eventos/src/components/Navbar/Navbar.js
+++ eventos/src/components/Navbar/Navbar.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {logout, reset} from '../../features/auth/authSlice'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user} = useSelector((state)=> state.auth)
+
+  const clickHandle = ()=>{
+    dispatch(logout());
+    dispatch(reset())
+    navigate('/')
+  }
+
   return (
     <nav className='bg-nvbg h-14'>
         <div className='flex justify-between'>
             <div>
                 Eventos
             </div>
+            {
+              user && <button onClick={clickHandle}>logout</button>
+            }
             <div>
                 profile
-            </div>    
+            </div>
+            {user && 
+            <Link to='/create'>create</Link>
+            }    
         </div>
     </nav>
   )
