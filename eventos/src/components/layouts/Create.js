0c1594ee51991149ff66945eca8f1c9cbe0b0b80
neeraj <neerajramachandran30@gmail.com>
2022-07-21 01:26:40 +0530

    layout for event creation form

---
+++ eventos/src/components/layouts/Create.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import EventForm from '../Event/EventForm'
+import Navbar from '../Navbar/Navbar'
+
+function Create() {
+  return (
+    <div>
+        <Navbar/>
+        <EventForm/>
+    </div>
+  )
+}
+
+export default Create
