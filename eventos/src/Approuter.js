8bc72e59389be66ce303d3c6a0be05679ad711fd
neeraj <neerajramachandran30@gmail.com>
2022-07-21 01:25:59 +0530

    create route added

--- eventos/src/Approuter.js
+++ eventos/src/Approuter.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Create from './components/layouts/Create'
 import Home from './components/layouts/Home'
 import Login from './components/layouts/Login'
 import Signup from './components/layouts/Signup'
@@ -10,6 +11,7 @@ function Approuter() {
         <Route exact path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        <Route path='/create' element={<Create/>}/>
       </Routes>   
     </Router>
   )
