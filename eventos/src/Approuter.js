ea688a4f6852866d0cfff25f88fb529de92e8b33
neeraj <neerajramachandran30@gmail.com>
2022-07-22 02:27:24 +0530

    created protected router

--- eventos/src/Approuter.js
+++ eventos/src/Approuter.js
@@ -4,6 +4,9 @@ import Create from './components/layouts/Create'
 import Home from './components/layouts/Home'
 import Login from './components/layouts/Login'
 import Signup from './components/layouts/Signup'
+import PrivateRoutes from './components/Route/PrivateRoutes'
+
+
 function Approuter() {
   return (
     <Router>
@@ -11,7 +14,10 @@ function Approuter() {
         <Route exact path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
-        <Route path='/create' element={<Create/>}/>
+        {/* <Route path='/create' element={<Create/>}/> */}
+        <Route element={<PrivateRoutes/>}>
+              <Route path='/create' element={<Create/>} />
+        </Route>
       </Routes>   
     </Router>
   )
