f5d270df7a9ce1101927a852b9cbbd68f9136481
neeraj <neerajramachandran30@gmail.com>
2022-07-24 19:03:06 +0530

    side navbar call setup

--- eventos/src/components/layouts/Home.js
+++ eventos/src/components/layouts/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Events from '../Event/Events'
 import Navbar from '../Navbar/Navbar'
+import Sidenav from '../Navbar/Sidenav'
 
 function Home() {
   return (
     <div>
         <Navbar/>
+        <Sidenav/>
         <Events/>
     </div>
   )
