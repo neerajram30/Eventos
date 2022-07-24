a59062ee32e916f8b5e061537c892cb8cee6d4b6
neeraj <neerajramachandran30@gmail.com>
2022-07-24 19:02:31 +0530

    my event button created

---
+++ eventos/src/components/assets/icons/Myevents.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { UserIcon } from '@heroicons/react/outline'
+function Myevents() {
+  return (
+    <div className='text-start'>
+        <button className='hover:bg-nvbg w-24 text-center items-center flex flex-col p-1 hover:rounded-lg'>
+        <UserIcon className='text-blue stroke-1 w-10'/>
+        <p className='text-sm' >my events</p>
+        </button>
+
+    </div>
+  )
+}
+
+export default Myevents
