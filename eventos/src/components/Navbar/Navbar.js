b4ef49de86c7f0ec62d2d4280adc4e9e22d51ff2
neeraj <neerajramachandran30@gmail.com>
2022-07-24 19:03:26 +0530

    some updates in navbar

--- eventos/src/components/Navbar/Navbar.js
+++ eventos/src/components/Navbar/Navbar.js
@@ -1,37 +1,17 @@
 import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
-import {logout, reset} from '../../features/auth/authSlice'
 import {Link} from 'react-router-dom'
-import Profile from '../assets/icons/Profile'
 import Dropdown from '../assets/icons/Dropdown'
 
 function Navbar() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  const {user} = useSelector((state)=> state.auth)
-
-  const clickHandle = ()=>{
-    dispatch(logout());
-    dispatch(reset())
-    navigate('/')
-  }
 
   return (
-    <nav className='bg-nvbg h-14 font-montserrat w-screen'>
+    <nav className='bg-nvbg h-14 font-montserrat'>
         <div className='flex justify-between'>
             <div className='pt-2 ml-5 font-inter text-xl'>
               <Link to='/'>
                 Eventos
               </Link>
-            </div>
-            {
-              user && <button onClick={clickHandle}>logout</button>
-            }
-            {user && 
-            <Link to='/create'>create</Link>
-            }    
+            </div>  
             <div className='pt-2 mr-5'>
               <Dropdown/>
             </div>
