3aa2df634c08023a8d513a1d076853cc8e47cf86
neeraj <neerajramachandran30@gmail.com>
2022-07-24 19:01:48 +0530

    dropdown button created in navbar

--- eventos/src/components/assets/icons/Dropdown.js
+++ eventos/src/components/assets/icons/Dropdown.js
@@ -1,15 +1,24 @@
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon,UserIcon } from '@heroicons/react/solid'
-import {useSelector} from 'react-redux'
-import { Link } from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../../../features/auth/authSlice'
+import {useNavigate, Link} from 'react-router-dom'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Example() {
-    const {user} = useSelector((state)=> state.auth)
+    const {user} = useSelector((state)=> state.auth);
+    const dispatch = useDispatch();
+    const navigate =useNavigate();
+    const clickHandle = ()=>{
+      dispatch(logout());
+      dispatch(reset())
+      navigate('/')
+    }
+
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
@@ -29,7 +38,7 @@ export default function Example() {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
-          <div className="py-1">
+          <div className="py-1"> 
             <Menu.Item>
               {({ active }) => (
                 <a
@@ -114,37 +123,37 @@ export default function Example() {
             </Menu.Item>
           </div>
           <div className="py-1">
-            <Menu.Item>
-              {user ?<div>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  logout
-                </a>
-              )}
-              </div>:
-              <div>
-
-              {({ active }) => (
-                  <a
-                  href="#"
-                  className={classNames(
+            {user ?
+              (<Menu.Item>
+                {({ active }) => (
+                  <button
+                  onClick={clickHandle}
+                    className={classNames(
                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                       'block px-4 py-2 text-sm'
-                      )}
-                      >
-                  logout
-                </a>
+                    )}
+                  >
+                   logout
+                  </button>
+                )}
+
+              </Menu.Item>)
+              :
+              (<Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to="/login"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    login
+                  </Link>
+                )}
+
+              </Menu.Item>
               )}
-              </div>
-              
-              }
-            </Menu.Item>
           </div>
         </Menu.Items>
       </Transition>
