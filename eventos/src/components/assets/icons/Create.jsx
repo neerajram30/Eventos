68ac9f286af1de0baf44f4580419ccd1aaef8a83
neeraj <neerajramachandran30@gmail.com>
2022-07-26 00:42:37 +0530

    Created button updated

--- eventos/src/components/assets/icons/Create.jsx
+++ eventos/src/components/assets/icons/Create.jsx
@@ -1,26 +1,21 @@
 import React from 'react'
 import { PencilAltIcon } from '@heroicons/react/outline'
-import {useNavigate} from 'react-router-dom'
-
 
 function Create() {
-  const navigate = useNavigate()
-  const handleClick = ()=>{
-    navigate('/create')
-
-  }
-
+  
 
 
   return (
     <div>
+      <a href='/create'>
       <button  
       className='hover:bg-nvbg w-24 text-center items-center flex flex-col p-1 hover:rounded-lg'
-      onClick={handleClick}
       >
+
         <PencilAltIcon className='text-blue stroke-1 w-10'/>
         <p className='text-sm' >create</p>
       </button>
+    </a>
     </div>
   )
 }
