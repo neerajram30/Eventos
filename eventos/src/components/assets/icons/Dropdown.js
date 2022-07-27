27e05c451f32c5f94fa054365fe23cfcc8e0cee8
neeraj <neerajramachandran30@gmail.com>
2022-07-27 20:15:24 +0530

    dropdwon menu

--- eventos/src/components/assets/icons/Dropdown.js
+++ eventos/src/components/assets/icons/Dropdown.js
@@ -22,7 +22,7 @@ export default function Example() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-white focus:outline-none">
+        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-blue shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-white focus:outline-none">
           <UserIcon className='w-6 pr-1' aria-hidden="true"/>
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
