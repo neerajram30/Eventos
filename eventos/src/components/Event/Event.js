a26c8686a33b0c6d338e5111d0fd8ecc399c326a
neeraj <neerajramachandran30@gmail.com>
2022-07-27 20:15:42 +0530

    updated div to link

--- eventos/src/components/Event/Event.js
+++ eventos/src/components/Event/Event.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Event({event,i}) {
   return (
-    <div key={i} className='shadow-[#ada6a63d] shadow-eventbox rounded-md p-6 h-64 mt-5'>
+
+    <Link to={event._id} key={i} className='shadow-[#ada6a63d] shadow-eventbox rounded-md p-6 h-64 mt-5'>
               <h2 className='text-2xl font-sora'>{event.title}</h2>
               <div className='mt-2 w-3/4 break-words'>
               <p className='font-thin text-sm'>{event.description}</p>
@@ -19,7 +21,7 @@ function Event({event,i}) {
               <div className='flex justify-end mt-10 '>
                 <button className=' p-2 text-white text-sm'>Register</button>
               </div>
-    </div>
+    </Link>
   )
 }
 
