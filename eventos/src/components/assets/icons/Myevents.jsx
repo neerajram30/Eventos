4a29c9ff5a9b6cfe9ecfd8049e1be3a7feea45dc
neeraj <neerajramachandran30@gmail.com>
2022-07-29 17:40:20 +0530

    small change

--- eventos/src/components/assets/icons/Myevents.jsx
+++ eventos/src/components/assets/icons/Myevents.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { UserIcon } from '@heroicons/react/outline'
 
 function Myevents() {
+  const [clicked,setClicked] = useState(false)
   return (
     <div className='text-start'>
       <a href="/myevent">
-        <button className='hover:bg-nvbg w-24 text-center items-center flex flex-col p-1 hover:rounded-lg'>
+        <button 
+        className='hover:bg-nvbg w-24 text-center items-center flex flex-col p-1 hover:rounded-lg'
+        >
         <UserIcon className='text-blue stroke-1 w-10'/>
         <p className='text-sm' >my events</p>
         </button>
