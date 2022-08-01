232fa4d7683ad3b466ba94b56971e441b3109dc8
neeraj <neerajramachandran30@gmail.com>
2022-08-01 19:37:11 +0530

    button crated for my events

--- eventos/src/components/Event/Event.js
+++ eventos/src/components/Event/Event.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {CalendarIcon, LocationMarkerIcon, TrashIcon, PencilAltIcon} from '@heroicons/react/solid'
-import {useSelector} from 'react-redux'
+import {CalendarIcon, LocationMarkerIcon, TrashIcon, PencilAltIcon} from '@heroicons/react/solid' 
+import {useDispatch} from 'react-redux'
+import {deleteEvent} from '../../features/event/eventSlice'
+import {useNavigate} from 'react-router-dom'
 
-function Event({event,i}) {
-  const {user} = useSelector((state)=> state.auth);
+function Event({event,i,user}) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const deleteClickhandle = ()=>{
+    dispatch(deleteEvent(event._id)).then((response)=>{
+      
+    console.log(response)
+    navigate('/myevent')
+    })
+  }
+
+
+
   return (
 
-    <div  key={i} className='shadow-[#ada6a63d] shadow-eventbox rounded-md p-6 h-64 mt-5'>
+    <div  key={i} className='shadow-[#ada6a63d] shadow-eventbox rounded-md p-6 h-64 mt-10'>
       <Link to={event._id}>
               <h2 className='text-2xl font-montserrat font-[700]'>{event.title}</h2>
               <div className='mt-2 w-3/4 break-words'>
@@ -25,9 +38,18 @@ function Event({event,i}) {
               </div>
               { user &&
               <div className='flex  justify-end items-end pr-12 pt-10'>
-                <div className='flex text-blue'>
-                <button className='w-7'><PencilAltIcon/></button>
-                <button className='w-7 ml-5'><TrashIcon/></button>
+                <div className='flex text-btnblue'>
+                <button className='w-24 h-10 border-[1px] border-btnblue flex justify-center pl-2 pr-2 items-center rounded-sm'>
+                <p className='text-[#000] font-montserrat font-[700] '>Edit</p>
+                  <PencilAltIcon className='text-[#000] w-4 ml-1'/>
+                  </button>
+                <button 
+                className='w-24 h-10 ml-5 bg-btnblue flex justify-center items-center pl-2 pr-2 rounded-sm'
+                onClick={deleteClickhandle}
+                >
+                  <p className='text-white font-montserrat font-[700] text-sm'>Delete</p>
+                  <TrashIcon className='text-white w-4 ml-1'/>
+                </button>
                 </div>
               </div>}
       </Link>
