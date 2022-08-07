afbb5fb94a88fd401fbedf57a6b12b1330a21328
neeraj <neerajramachandran30@gmail.com>
2022-08-07 13:39:23 +0530

    icons change

--- eventos/src/components/assets/icons/Create.jsx
+++ eventos/src/components/assets/icons/Create.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PencilAltIcon } from '@heroicons/react/outline'
+import { PencilIcon } from '@heroicons/react/outline'
 
 function Create() {
   
@@ -12,7 +12,7 @@ function Create() {
       className='hover:bg-nvbg w-24 text-center items-center flex flex-col p-1 hover:rounded-lg'
       >
 
-        <PencilAltIcon className='text-blue stroke-1 w-10'/>
+        <PencilIcon className='text-blue stroke-1 w-10'/>
         <p className='text-sm' >create</p>
       </button>
     </a>
